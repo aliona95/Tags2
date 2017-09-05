483ea8bc13fca8100b6918b2a851c64dd096d83f
aliona95 <alionalisovskaja95@gmail.com>
2017-09-05 11:10:39 +0300

    search

--- app/src/main/java/com/aeappss/hashtags/MainActivity.java
+++ app/src/main/java/com/aeappss/hashtags/MainActivity.java
@@ -6,14 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.aeappss.hashtags.Tab.MyAdapter;
 import com.aeappss.hashtags.Tab.SlidingTabLayout;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.miguelcatalan.materialsearchview.SearchAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private SlidingTabLayout mSlidingTabLayout;
     private ViewPager mViewPager;
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        searchViewCode();
 
         mViewPager = (ViewPager) findViewById(R.id.vp_tabs);
         mViewPager.setAdapter(new MyAdapter(getSupportFragmentManager(), this));
@@ -38,7 +46,56 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
         return true;
+    }
+
+    private void searchViewCode(){
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        //searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+        searchView.setEllipsize(true);
+
+        String [] result = {"labas","ate","miau"};
+        searchView.setAdapter(new SearchAdapter(this, result));
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Toast.makeText(getApplicationContext(), query + " SUBMIT", Toast.LENGTH_SHORT).show();
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                //Toast.makeText(getApplicationContext(), newText + "CHANGE", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+                //Toast.makeText(getApplicationContext(), "AS CIA2 SHOW", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+                //Toast.makeText(getApplicationContext(), "AS CIA2 CLOSE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        searchView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Paspausta suggestion" + "pozicija" + position + "id" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
